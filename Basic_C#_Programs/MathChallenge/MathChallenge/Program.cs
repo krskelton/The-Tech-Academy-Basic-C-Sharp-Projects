4f13eeebab809cb84eb6d8984095621f123645b5
Kristin Skelton <kskelton82@gmail.com>
2021-03-06 10:55:18 -0700

    Math Operations
    Demonstrate various math operations

---
+++ Basic_C#_Programs/MathChallenge/MathChallenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            //add two numbers
+            int num1 = 2;
+            int num2 = 4;
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+
+            //subtract two numbers
+            int num3 = 3;
+            int num4 = 5;
+            int subNum = num3 - num4;
+            Console.WriteLine(subNum);
+
+            //multiply two numbers
+            int num5 = 5;
+            int num6 = 10;
+            int mulNum = num5 * num6;
+            Console.WriteLine(mulNum);
+
+            //Divide two numbers
+            int num7 = 10;
+            int num8 = 2;
+            int divNum = num7 / num8;
+            Console.WriteLine(divNum);
+        }
+    }
+}
