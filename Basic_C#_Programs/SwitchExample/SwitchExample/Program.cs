d1763e76ff9e6739fe279ad627f653c7b8e1ee63
Kristin Skelton <kskelton82@gmail.com>
2021-03-09 14:17:35 -0700

    Example of Switch statement
    Switch statement in C#

---
+++ Basic_C#_Programs/SwitchExample/SwitchExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SwitchExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            switch (number) {
+                case 1:
+                    Console.WriteLine("You chose one of my numbers!");
+                    break;
+                case 5:
+                    Console.WriteLine("You chose one of my numbers!");
+                    break;
+                case 10:
+                    Console.WriteLine("You chose one of my numbers!");
+                    break;
+                default:
+                    Console.WriteLine("Sorry, you did not choose one of my numbers!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
