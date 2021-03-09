f29144ad7472fead7140d692f937917f91e1b58e
Kristin Skelton <kskelton82@gmail.com>
2021-03-09 12:17:19 -0700

    If Else example
    Determine if a condition is true and display results based on the value

---
+++ Basic_C#_Programs/IfElseExample/IfElseExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IfElseExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you a dog? Please answer \"true\" or \"false\".");
+            bool isDog = Convert.ToBoolean(Console.ReadLine());
+            if (isDog)
+            {
+                Console.WriteLine("Woof Woof!");
+            }
+            else {
+                Console.WriteLine("You wouldn't understand if you aren't a dog.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
