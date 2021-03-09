60ae7396043cfb2c644fbcb0c3e729a91c591b9a
Kristin Skelton <kskelton82@gmail.com>
2021-03-09 12:31:15 -0700

    Example of the Not Operator
    Example using if else statements with the not operator.

---
+++ Basic_C#_Programs/NotOperator/NotOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NotOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you a cat? Please respond \"true\" or \"false\".");
+            bool isCat = Convert.ToBoolean(Console.ReadLine());
+            if (isCat != true)
+            {
+                Console.WriteLine("Cats are cool. You should be a cat!");
+            }
+            else {
+                Console.WriteLine("I love that you are a cat!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
