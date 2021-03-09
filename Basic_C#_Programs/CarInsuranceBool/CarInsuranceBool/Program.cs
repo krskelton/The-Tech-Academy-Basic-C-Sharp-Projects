2d8e6c6c8197cfa26360535cfa36e022d3c1a37d
Kristin Skelton <kskelton82@gmail.com>
2021-03-09 12:05:13 -0700

    Car Insurance Boolean Example
    Determine if a user is eligible for car insurance based on boolean arguments.

---
+++ Basic_C#_Programs/CarInsuranceBool/CarInsuranceBool/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CarInsuranceBool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determine age of user
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //Determine if user had a DUI
+            Console.WriteLine("Have you ever had a DUI? Please answer either \"true\" or \"false\".");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            //Determine if user has any speeding tickets
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            //Determine if the user is eligible for car insurance
+            //Age must be over 15
+            //No DUIs
+            //No more than 3 speeding tickets
+            bool isEligible = age > 15 && dui == false && tickets <= 3;
+            Console.WriteLine("Qualified?\n" + isEligible);
+            Console.ReadLine();
+        }
+    }
+}
