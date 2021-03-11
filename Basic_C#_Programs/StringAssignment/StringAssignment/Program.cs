ff0ee74342406b2a2b133a9759f2c06644b8503c
Kristin Skelton <kskelton82@gmail.com>
2021-03-11 11:30:14 -0700

    String functions
    Demonstrate string concatenation and StringBuilder.

---
+++ Basic_C#_Programs/StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Concatenate three strings
+            string dog = "Cali";
+            string cat = "Bailey";
+            string daughter = "Cameron";
+            string myFamily = "My family is made up of " + dog + ", " + cat + ", " + "and " + daughter + ".";
+            Console.WriteLine(myFamily);
+
+            //Convert string to uppercase
+            string boyfriend = "Seth";
+            Console.WriteLine("There's also a cute boy named " + boyfriend.ToUpper() + "!");
+
+            //Create a StringBuilder and build a paragraph
+            StringBuilder paragraph = new StringBuilder();
+            paragraph.Append("This is a paragraph about my family. ");
+            paragraph.Append("I love them very much! ");
+            paragraph.Append("They are the sweetest people and pets on earth. ");
+            Console.WriteLine(paragraph);
+
+            Console.ReadLine();
+        }
+    }
+}
