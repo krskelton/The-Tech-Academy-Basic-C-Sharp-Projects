acc923811957a84199df2c4589e9e574ce1c2953
Kristin Skelton <kskelton82@gmail.com>
2021-03-13 11:17:05 -0700

    Iteration Examples
    Demonstrate how to iterate through an int array and a string array.

---
+++ Basic_C#_Programs/IterationExample/IterationExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IterationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+            for (int i = 0; i < testScores.Length; i++) {
+                if (testScores[i] > 85) {
+                    Console.WriteLine("Passing test score: " + testScores[i]);
+                }
+            }
+            //Console.ReadLine();
+
+            string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+            for (int j = 0; j < names.Length; j++) {
+                if (names[j] == "Jesse") {
+                    Console.WriteLine(names[j]);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
