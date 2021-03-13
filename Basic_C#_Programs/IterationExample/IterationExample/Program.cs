6fe914675556a21c00898bcb8b57af279f4e87ce
Kristin Skelton <kskelton82@gmail.com>
2021-03-13 11:43:25 -0700

    Add List example
    Add example of iterating through a List

--- Basic_C#_Programs/IterationExample/IterationExample/Program.cs
+++ Basic_C#_Programs/IterationExample/IterationExample/Program.cs
@@ -18,10 +18,37 @@ namespace IterationExample
             string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
 
             for (int j = 0; j < names.Length; j++) {
-                if (names[j] == "Jesse") {
-                    Console.WriteLine(names[j]);
+                Console.WriteLine(names[j]);
+            }
+
+            List<int> scores = new List<int>();
+            scores.Add(98);
+            scores.Add(99);
+            scores.Add(81);
+            scores.Add(72);
+            scores.Add(70);
+
+            foreach (int score in scores) {
+                if (score > 85) {
+                    Console.WriteLine("Passing test score: " + score);
                 }
             }
+
+            List<string> namesList = new List<string>() { "Jesse", "Erik", "Adam", "Daniel" };
+
+            foreach (string name in namesList) {
+                Console.WriteLine(name);
+            }
+
+            List<int> testScores2 = new List<int>() { 98, 99, 12, 74, 23, 99 };
+            List<int> passingScores = new List<int>();
+
+            foreach (int score2 in testScores2) {
+                if (score2 > 85) {
+                    passingScores.Add(score2);
+                }
+            }
+            Console.WriteLine(passingScores.Count);
 
             Console.ReadLine();
         }
