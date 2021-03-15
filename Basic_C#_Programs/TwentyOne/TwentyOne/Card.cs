37b0693d91c30b66ac1391531119039edc2bc944
Kristin Skelton <kskelton82@gmail.com>
2021-03-15 11:34:52 -0600

    Add Card and Deck classes
    Create classes for a card object and a deck object

---
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        //constructor - it's always the name of the class
+        public Card() 
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
