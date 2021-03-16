046426c2d6f622136d6b33ea840fae48804dae8a
Kristin Skelton <kskelton82@gmail.com>
2021-03-16 12:22:21 -0600

    Add methods to Twenty One program
    Note: had to redo the program because of some errors, so this one is named v2.

---
+++ Basic_C#_Programs/TwentyOnev2/TwentyOnev2/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOnev2
+{
+    public class Card
+    {
+        //constructor - always the name of the class
+        public Card()
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
--- Basic_C#_Programs/TwentyOnev2/TwentyOnev2/Program.cs
+++ Basic_C#_Programs/TwentyOnev2/TwentyOnev2/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TwentyOne
+namespace TwentyOnev2
 {
     class Program
     {
@@ -36,3 +36,4 @@ namespace TwentyOne
         }
     }
 }
+
