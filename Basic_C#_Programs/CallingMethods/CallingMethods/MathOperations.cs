401663601562bd12afb71d1c5bda051d0732afd6
Kristin Skelton <kskelton82@gmail.com>
2021-03-16 12:56:31 -0600

    Create a class that performs math operations on an integer input by the user

--- Basic_C#_Programs/CallingMethods/CallingMethods/MathOperations.cs
+++ Basic_C#_Programs/CallingMethods/CallingMethods/MathOperations.cs
@@ -6,6 +6,20 @@ namespace CallingMethods
 {
     public class MathOperations
     {
-
+        //This method adds 1 to the integer passed to it
+        public int AddInt(int userResponse)
+        {
+            return userResponse + 1;
+        }
+        //This method multiplies the integer by itself
+        public int MultiplyInt(int userResponse)
+        {
+            return userResponse * userResponse;
+        }
+        //This method divides the integer provided by 2
+        public int DivideInt(int userReponse)
+        {
+            return userReponse / 2;
+        }
     }
 }
--- Basic_C#_Programs/CallingMethods/CallingMethods/Program.cs
+++ Basic_C#_Programs/CallingMethods/CallingMethods/Program.cs
@@ -6,7 +6,22 @@ namespace CallingMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create a new MathOperations object
+            MathOperations math = new MathOperations();
+            Console.WriteLine("What number would you like to perform some math operations on?");
+            //save response as an integer
+            int response = Convert.ToInt32(Console.ReadLine());
+
+            //Call each math operation, passing them the user's reponse
+            Console.WriteLine("When I add 1 to " + response + ", I get: " + math.AddInt(response));
+            Console.WriteLine("When I multiply " + response + " by itself, I get: " + math.MultiplyInt(response));
+            Console.WriteLine("When I divide " + response + " by 2, I get: " + math.DivideInt(response));
+
+        }
+
+        private static void AddInt(int response)
+        {
+            throw new NotImplementedException();
         }
     }
 }
