bf392f168d9eba220c84c1657e91e128d49f7daa
Kristin Skelton <kskelton82@gmail.com>
2021-03-16 12:25:14 -0600

    Initial commit of Methods

---
+++ Basic_C#_Programs/CallingMethods/CallingMethods/MathOperations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethods
+{
+    public class MathOperations
+    {
+
+    }
+}
---
+++ Basic_C#_Programs/CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
