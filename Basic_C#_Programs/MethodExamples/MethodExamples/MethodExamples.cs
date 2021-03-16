e21b2b1d7c9eaad4387fe63a1b76a4ca004e3852
Kristin Skelton <kskelton82@gmail.com>
2021-03-16 14:42:15 -0600

    Examples of method overloading

---
+++ Basic_C#_Programs/MethodExamples/MethodExamples/MethodExamples.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodExamples
+{
+    public class MethodExamples
+    {
+        //method that takes an integer, performs math operation and returns the answer
+        public int MathOperation(int integer)
+        {
+            //add 2 to the integer passed in
+            return integer + 2;
+        }
+        //method with same name that takes in a decimal, perform a different math operation and returns the answer
+        public int MathOperation(decimal dec)
+        {
+            //multiply the decimal by 2
+            decimal multiply = dec * 2.5m;
+            //return an integer
+            return Convert.ToInt32(multiply);
+        }
+        //method with same name, takes a string, converts it into an integer and does another math operation
+        public int MathOperation(string stringInt)
+        {
+            //Convert the string to an integer
+            int intString = Convert.ToInt32(stringInt);
+            //divide the integer by 2
+            return intString / 2;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodExamples/MethodExamples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate a MethodExamples object
+            MethodExamples example = new MethodExamples();
+            //Output the result of calling the first MathOperation method
+            Console.WriteLine("When I add 2 to the integer 2, I get: " + example.MathOperation(2));
+            //Output result of calling the second MathOperation method
+            Console.WriteLine("When I multiply the decimal 2.5 by 2.5, I get: " + example.MathOperation(2.5m) + ", because I've returned the value as an integer");
+            //Output results of calling the third MathOperation method
+            Console.WriteLine("When I divide the string 4 by 2, I get: " + example.MathOperation("4") + ", because I changed it to an integer.");
+        }
+    }
+}
