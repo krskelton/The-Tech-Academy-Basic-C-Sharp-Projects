6218798ef42fcafde656ca5a731affb4653b3964
Kristin Skelton <kskelton82@gmail.com>
2021-03-16 15:28:27 -0600

    Create void methods and pass parameters with names

---
+++ Basic_C#_Programs/NamingParameters/NamingParameters/NamingParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NamingParameters
+{
+    public class NamingParameters
+    {
+        public static void Naming(int num1, int num2)
+        {
+            //perform a math operation on the first parameter
+            int newValue = num1 * 2;
+
+            Console.WriteLine("The second number entered is: " + num2);
+        }
+    }
+}
---
+++ Basic_C#_Programs/NamingParameters/NamingParameters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NamingParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            NamingParameters naming = new NamingParameters();
+            //call the method, specify the parameters by name
+            NamingParameters.Naming(num1: 2, num2: 4);
+            Console.ReadLine();
+        }
+    }
+}
