962af2d64deb34d5380ecb5f88774eb57c4693c4
Kristin Skelton <kskelton82@gmail.com>
2021-03-17 12:42:51 -0600

    Move Shuffle method to class

--- Basic_C#_Programs/TwentyOnev2/TwentyOnev2/Deck.cs
+++ Basic_C#_Programs/TwentyOnev2/TwentyOnev2/Deck.cs
@@ -28,5 +28,23 @@ namespace TwentyOnev2
             }
         }
         public List<Card> Cards { get; set; }
+        //The times parameter here is optional because it has a default value. 
+        //So if someone doesn't pass it when the method is called, it still has a value
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+            }
+        }
     }
 }
--- Basic_C#_Programs/TwentyOnev2/TwentyOnev2/Program.cs
+++ Basic_C#_Programs/TwentyOnev2/TwentyOnev2/Program.cs
@@ -11,37 +11,16 @@ namespace TwentyOnev2
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-            int timesShuffled = 0;
-            deck = Shuffle(deck: deck, out timesShuffled, times: 3);
+            deck.Shuffle();
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
-            Console.WriteLine("Times shuffled: {0}", timesShuffled);
             Console.ReadLine();
         }
-        //The times parameter here is optional because it has a default value. 
-        //So if someone doesn't pass it when the method is called, it still has a value
-        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
-        {
-            timesShuffled = 0;
-            for (int i = 0; i < times; i++) {
-                timesShuffled++;
-                List<Card> TempList = new List<Card>();
-                Random random = new Random();
-
-                while (deck.Cards.Count > 0)
-                {
-                    int randomIndex = random.Next(0, deck.Cards.Count);
-                    TempList.Add(deck.Cards[randomIndex]);
-                    deck.Cards.RemoveAt(randomIndex);
-                }
-                deck.Cards = TempList;
-            }
-            return deck;
-        }
+        
     }
 }
 
