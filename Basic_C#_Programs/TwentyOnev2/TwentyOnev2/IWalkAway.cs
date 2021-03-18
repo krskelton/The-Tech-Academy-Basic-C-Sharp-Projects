f7b8a3e9a36ca4ffa5b7ee0ff5a01c221ca66859
Kristin Skelton <kskelton82@gmail.com>
2021-03-18 13:45:31 -0600

    Add interface to allow the user's to walk away from the game

---
+++ Basic_C#_Programs/TwentyOnev2/TwentyOnev2/IWalkAway.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOnev2
+{
+    interface IWalkAway
+    {
+        //There must be some way for the user to stand up and walk away in this game
+        void WalkAway(Player player);
+    }
+}
--- Basic_C#_Programs/TwentyOnev2/TwentyOnev2/TwentyOneGame.cs
+++ Basic_C#_Programs/TwentyOnev2/TwentyOnev2/TwentyOneGame.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace TwentyOnev2
 {
-    class TwentyOneGame : Game
+    class TwentyOneGame : Game, IWalkAway
     {
         public override void Play()
         {
@@ -14,6 +14,10 @@ namespace TwentyOnev2
         {
             Console.WriteLine("21 Players:");
             base.ListPlayers();
+        }
+        public void WalkAway(Player player)
+        {
+            throw new NotImplementedException();
         }
     }
 }
