cb55751173bd6979756e749cb2ef5225f2311b42
Kristin Skelton <kskelton82@gmail.com>
2021-03-19 11:50:09 -0600

    Generic list in class

---
+++ Basic_C#_Programs/Parameters/Parameters/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parameters
+{
+    public class Employee<T>
+    {
+        //create a generic list
+        public List<T> Things { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/Parameters/Parameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an employee list with string data type
+            Employee<string> stringThings = new Employee<string>();
+            stringThings.Things = new List<string>();
+            stringThings.Things.Add("thing1");
+            stringThings.Things.Add("thing2");
+            stringThings.Things.Add("Dr. Seuss");
+
+            //create an employee list with int data type
+            Employee<int> intThings = new Employee<int>();
+            intThings.Things = new List<int>();
+            intThings.Things.Add(1);
+            intThings.Things.Add(2);
+            intThings.Things.Add(3);
+
+            //print items
+            foreach (string thing in stringThings.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            foreach (int thing in intThings.Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
