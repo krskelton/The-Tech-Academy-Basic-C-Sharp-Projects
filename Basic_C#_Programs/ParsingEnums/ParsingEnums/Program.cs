0315e1a2c73f774ffa23b1b0987b358e1878e255
Kristin Skelton <kskelton82@gmail.com>
2021-03-19 12:34:26 -0600

    Create enum, convert string into an enum

---
+++ Basic_C#_Programs/ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user the day of the week
+            Console.WriteLine("What is the current day of the week?");
+            //try to convert that input into an enum
+            try
+            {
+                //get the user's input
+                string userInput = Console.ReadLine();
+                //convert the string to an enum
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userInput);
+                //Output the user's input
+                Console.WriteLine("You entered: " + day);
+            }
+            //if there are errors display a response to the user
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+            
+        }
+        //define enum for days of the week
+        public enum DaysOfTheWeek
+        { 
+            Monday, 
+            Tuesday, 
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
