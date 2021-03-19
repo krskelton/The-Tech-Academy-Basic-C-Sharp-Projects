333c1a8bfadc0f6b992b893bd8cab4d5749efefc
Kristin Skelton <kskelton82@gmail.com>
2021-03-19 13:13:26 -0600

    Struct example

---
+++ Basic_C#_Programs/Struct/Struct/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Struct
+{
+    //create a struct
+    public struct Number
+    {
+        //property amount of type decimal
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/Struct/Struct/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a new Number object and assign a value to Amount
+            Number number = new Number();
+            number.Amount = 10.50m;
+            //Print the amount
+            Console.WriteLine(number.Amount);
+
+            Console.ReadLine();
+        }
+    }
+}
