34c60bd71f1c76a228f1b2b3fb3c6aadcd4ef332
Kristin Skelton <kskelton82@gmail.com>
2021-03-24 11:59:16 -0600

    Add hours to a DateTime object

---
+++ Basic_C#_Programs/DateTime/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace DateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get the current time and date
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine("Current date and time: " + currentDate);
+            //Ask the user for a number
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            //Add the number to the currentDate
+            DateTime laterTime = currentDate.AddHours(number);
+            //Print the time it will be in x hours, defined by the number the user entered
+            Console.WriteLine("In " + number + " hours, it will be: " + laterTime);
+        }
+    }
+}
