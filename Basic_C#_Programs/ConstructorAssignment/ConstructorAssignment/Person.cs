f3b4c2d35229e51d0db6a8759c1b2509b54a9d81
Kristin Skelton <kskelton82@gmail.com>
2021-03-24 14:26:14 -0600

    Use const and var variables, and constructor overloading

---
+++ Basic_C#_Programs/ConstructorAssignment/ConstructorAssignment/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorAssignment
+{
+    public class Person
+    {
+        //constructor chaining
+        public Person(string lastName) : this(lastName, "Skelton")
+        { 
+        }
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //define a const variable
+            const string constantVariable = "Kristin Skelton";
+            //write the const variable to the console
+            Console.WriteLine("const variable is " + constantVariable);
+
+            //define a var variable
+            var varVariable = "This is some text who's data type will be implicity defined.";
+            //write the var variable to the console
+            Console.WriteLine("var variable is " + varVariable);
+
+            //Create a new person that uses the chained constructor
+            Person person = new Person("Kristin");
+            //write the person to the console
+            Console.WriteLine("I created a person from the Person class by only sending the first name: Kristin. This gives us: " + person.FirstName + " " + person.LastName);
+
+            Console.ReadLine();
+        }
+    }
+}
