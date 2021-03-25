6523ff5e765dce2feca0014efed1f5bd45f26005
Kristin Skelton <kskelton82@gmail.com>
2021-03-25 13:04:06 -0600

    Exception handling examples

---
+++ Basic_C#_Programs/TryCatchAssignment/TryCatchAssignment/InvalidAgeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryCatchAssignment
+{
+    public class InvalidAgeException : Exception
+    {
+        public InvalidAgeException()
+            : base() { }
+        public InvalidAgeException(string message)
+            : base(message) { }
+    }
+}
---
+++ Basic_C#_Programs/TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask use for their age
+            Console.WriteLine("Enter your age:");
+            //try to change the entry into an integer
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 0)
+                {
+                    throw new InvalidAgeException();
+                }
+                //get the current year and subtract the user's age
+                DateTime currentDate = DateTime.Now;
+                int year = currentDate.Year;
+                //get their birth year by subtracting the age from the current year. 
+                //Note: without the month and year they were born, this year could be off.
+                int birthYear = year - age;
+                Console.WriteLine(birthYear);
+                Console.ReadLine();
+            }
+            catch (InvalidAgeException)
+            {
+                Console.WriteLine("Please do not enter a zero or negative number");
+                Console.ReadLine();
+            }
+            //catch errors
+            catch (Exception)
+            {
+                Console.WriteLine("I'm sorry an error has occurred. Please contact your administrator");
+                Console.ReadLine();
+            }
+            
+            
+        }
+    }
+}
