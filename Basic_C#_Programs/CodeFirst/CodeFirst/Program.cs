d1014b5548005ef2887c76b874d6c9ab7f521a49
Kristin Skelton <kskelton82@gmail.com>
2021-04-05 08:31:12 -0600

    Code First app

---
+++ Basic_C#_Programs/CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { FirstName = "Kristin", LastName = "Skelton" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                Console.WriteLine("Student saved successfully!");
+
+                foreach (var student in ctx.Students)
+                {
+                    Console.WriteLine(student.FirstName + " " + student.LastName);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
